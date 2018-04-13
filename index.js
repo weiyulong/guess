0ef985b3767554737e2e809c094f26a658cc4f17
魏玉龙 <weiyulong@qq.com>
2018-04-13 18:15:24 +0800

    加入部分猜解流程

--- index.js
+++ index.js
@@ -30,7 +30,10 @@ inquirer.prompt([
         name: '困难',
         value: 'hard'
       }
-    ]
+    ],
+    when (answer) {
+      return answer.type === 'Player'
+    }
   },
   {
     type: 'confirm',
@@ -44,8 +47,10 @@ inquirer.prompt([
       let answer = generate(mode)
       question(answer)
     } else if (type === 'AI') {
-      console.log(JSON.stringify(answers, null, '  '));
+      console.log('真抱歉，我还没学会怎么猜呢！');
     }
+  } else {
+    console.log('那就等你准备好了再来找我吧！');
   }
 });
 
@@ -121,6 +126,101 @@ function question (answer) {
         return true
       } else {
         return '拜托！请输入4位数字好么？'
+      }
+    }
+  })
+}
+
+function guess () {
+  let round = 1
+  let member = {
+    '0': {
+      skip: new Set()
+    },
+    '1': {
+      skip: new Set()
+    },
+    '2': {
+      skip: new Set()
+    },
+    '3': {
+      skip: new Set()
+    },
+    '4': {
+      skip: new Set()
+    },
+    '5': {
+      skip: new Set()
+    },
+    '6': {
+      skip: new Set()
+    },
+    '7': {
+      skip: new Set()
+    },
+    '8': {
+      skip: new Set()
+    },
+    '9': {
+      skip: new Set()
+    }
+  }
+  let suspects = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+  let results = [{ answer: '1234' }]
+  let prompts = new Rx.Subject()
+  inquirer.prompt(prompts).ui.process.subscribe(
+    reply => {
+      let { name, answer } = reply
+      let validate = answer.match(/(\d)A(\d)B/)
+      results[round - 1].A = validate[1]
+      results[round - 1].B = validate[2]
+      if (answer === '4A0B') {
+        console.log('ok')
+        prompts.complete()
+      } else if (answer === '0A0B') {
+        for (num of results[round - 1].answer) {
+          member[num].out = true
+        }
+      } else if (validate[1] == 0 && validate[2] > 0) {
+        for (let i = 0; i < results[round - 1].answer.length; i++) {
+          member[results[round - 1].answer[i]].skip.add(i)
+        }
+      } else if (reply.answer === '0A2B') {
+
+      } else if (reply.answer === '0A3B') {
+
+      } else if (reply.answer === '0A4B') {
+
+      } else if (reply.answer === '1A3B') {
+
+      } else if (reply.answer === '2A2B') {
+
+      } else if (reply.answer === '3A0B') {
+
+      } else if (reply.answer === '2A1B') {
+
+      } else if (reply.answer === '2A0B') {
+
+      } else if (reply.answer === '1A2B') {
+
+      } else if (reply.answer === '1A1B') {
+
+      } else if (reply.answer === '1A0B') {
+
+      }
+    },
+    error => { },
+    () => { }
+  )
+  prompts.next({
+    type: 'input',
+    name: `第${round++}回合`,
+    message: '是1234吗？',
+    validate: value => {
+      if (/^[0-4]A[0-4]B$/.test(value)) {
+        return true
+      } else {
+        return '拜托！请给我正常的（?A?B）反馈好么？'
       }
     }
   })
